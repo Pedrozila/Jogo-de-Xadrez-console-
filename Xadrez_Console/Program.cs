4b2543f3150073f2d36bb51e702b00d50dc9fd1e
Pedro Bruno <pedrobn97@outlook.com>
2024-07-15 12:49:28 -0300

    Criada a primeira classe (Posicao)

--- Xadrez_Console/Program.cs
+++ Xadrez_Console/Program.cs
@@ -1,10 +1,12 @@
-﻿namespace Xadrez_Console
+﻿using Xadrez_Console.Tabuleiro;
+
+namespace Xadrez_Console
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Posicao P = new Posicao(3, 4);
         }
     }
 }
